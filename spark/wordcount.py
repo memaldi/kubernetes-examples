731ea05638beb8680c76a900675c1fd1f98879cf
Mikel Emaldi <m.emaldi@deusto.es>
2024-05-10 10:18:14 +0200

    Update wordcount.py

--- spark/wordcount.py
+++ spark/wordcount.py
@@ -5,17 +5,9 @@ conf = SparkConf()
 sc = SparkContext(conf=conf)
 spark = SparkSession.builder.getOrCreate()
 
-access_key = "access-key"
-secret_key = "secret-key"
 s3_bucket = "my-bucket"
-minio_endpoint = "http://minio.minio.svc.cluster.local:9000"
 input_path = "s3a://{}/input".format(s3_bucket)
 output_path = "s3a://{}/output".format(s3_bucket)
-
-spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", access_key)
-spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", secret_key)
-spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", minio_endpoint)
-spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
 
 input = sc.textFile(input_path)
 
